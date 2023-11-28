f3fdeaa63ac305ef1ee65938cc6175d5e821d08a
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-11-28 09:48:50 +0800

    Add new files

---
+++ app/src/main/java/com/example/mgp2023/SmurfEntity.java
@@ -0,0 +1,130 @@
+package com.example.mgp2023;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+import java.util.Random;
+
+public class SmurfEntity implements EntityBase, Collidable{
+    
+	 // 1. Declare the use of spritesheet using Sprite class.
+
+    private boolean isDone = false;
+    private boolean isInit = false;
+
+	 // Variables to be used or can be used.
+    public float xPos, yPos, xDir, yDir, lifeTime;
+    
+	 // For use with the TouchManager.class
+    private boolean hasTouched = false;
+
+    int ScreenWidth, ScreenHeight;
+
+    @Override
+    public boolean IsDone() {
+        return isDone;
+    }
+
+    @Override
+    public void SetIsDone(boolean _isDone) {
+        isDone = _isDone;
+    }
+
+    @Override
+    public void Init(SurfaceView _view) {
+        // New method using our own resource manager : Returns pre-loaded one if exists
+        // 2. Loading spritesheet
+        
+	
+		  // 3. Get some random position of x and y 
+        
+
+        isInit = true;
+
+    }
+
+    @Override
+    public void Update(float _dt) {
+        
+		  // 4. Update spritesheet
+
+        // 5. Deal with the touch on screen for interaction of the image using collision check
+        if (TouchManager.Instance.HasTouch())
+        {
+            // 6. Check collision here!!!
+            
+
+                // Collided!
+                hasTouched = true;
+
+                // 7. Drag the sprite around the screen
+
+            }
+        }
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas) {
+       
+        // This is for our sprite animation!
+        spritesheet.Render(_canvas, (int)xPos, (int)yPos);
+
+    }
+
+    @Override
+    public boolean IsInit() {
+        return isInit;
+    }
+
+    @Override
+    public int GetRenderLayer(){
+        return LayerConstants.SMURF_LAYER;
+    }
+
+    @Override
+    public void SetRenderLayer(int _newLayer)
+    {
+        return;
+    }
+
+    public static SmurfEntity Create()
+    {
+        SmurfEntity result = new SmurfEntity();
+        EntityManager.Instance.AddEntity(result, ENTITY_TYPE.ENT_SMURF);
+        return result;
+    }
+
+    @Override
+    public ENTITY_TYPE GetEntityType(){return ENTITY_TYPE.ENT_SMURF;}
+
+    @Override
+    public String GetType() {
+        return "SmurfEntity";
+    }
+
+    @Override
+    public float GetPosX() {
+        return xPos;
+    }
+
+    @Override
+    public float GetPosY() {
+        return yPos;
+    }
+
+    @Override
+    public float GetRadius() {
+        return spritesheet.GetHeight() * 0.5f;
+    }
+
+    @Override
+    public void OnHit(Collidable _other) {
+        //if (_other.GetType() == "StarEntity") //Another Entity
+        {
+            //SetIsDone(true);
+            //Play an audio
+        }
+    }
+
+}
