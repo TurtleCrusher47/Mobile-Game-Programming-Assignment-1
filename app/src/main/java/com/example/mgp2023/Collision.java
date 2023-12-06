34f080322e5e5aad4b2011bc166122acdaabe1cd
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-12-06 21:58:31 +0800

    Changed `BoxToBox` parameters

--- app/src/main/java/com/example/mgp2023/Collision.java
+++ app/src/main/java/com/example/mgp2023/Collision.java
@@ -18,11 +18,11 @@ public class Collision
         return true;
     }
 
-    public static boolean BoxToBox(float x1, float y1, float sizeX1, float sizeY1, float x2, float y2, float sizeX2, float sizeY2)
+    public static boolean BoxToBox(float x1, float y1, float width1, float height1, float x2, float y2, float width2, float height2)
     {
-        if (Math.abs(x1 - x2) < sizeX1 + sizeX2)
+        if (Math.abs(x1 - x2) < width1 + width2)
         {
-            if (Math.abs(y1 - y2) < sizeY1 + sizeY2)
+            if (Math.abs(y1 - y2) < height1 + height2)
             {
                 return true;
             }
