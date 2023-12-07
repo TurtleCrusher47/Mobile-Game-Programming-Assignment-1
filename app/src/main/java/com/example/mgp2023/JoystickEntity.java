651200fa4489eacd0c5666693c2195f9dbfd1375
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-12-08 01:10:21 +0800

    Add base functions to `JoystickEntity`

--- app/src/main/java/com/example/mgp2023/JoystickEntity.java
+++ app/src/main/java/com/example/mgp2023/JoystickEntity.java
@@ -1,4 +1,61 @@
 package com.example.mgp2023;
 
-public class JoystickEntity {
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class JoystickEntity implements EntityBase {
+
+    public JoystickEntity()
+    {
+        
+    }
+    @Override
+    public boolean IsDone() {
+        return false;
+    }
+
+    @Override
+    public void SetIsDone(boolean _isDone) {
+
+    }
+
+    @Override
+    public void Init(SurfaceView _view) {
+
+    }
+
+    @Override
+    public void Update(float _dt) {
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas) {
+
+    }
+
+    @Override
+    public boolean IsInit() {
+        return false;
+    }
+
+    @Override
+    public int GetRenderLayer() {
+        return 0;
+    }
+
+    @Override
+    public void SetRenderLayer(int _newLayer) {
+
+    }
+
+    @Override
+    public ENTITY_TYPE GetEntityType() {
+        return null;
+    }
+
+    @Override
+    public String GetType() {
+        return null;
+    }
 }
