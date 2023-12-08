e98c649bea2a744da8de067a7804972575da48fc
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-12-08 20:19:58 +0800

    Create base functions for `LoseScreen`

--- app/src/main/java/com/example/mgp2023/LoseScreen.java
+++ app/src/main/java/com/example/mgp2023/LoseScreen.java
@@ -1,4 +1,43 @@
 package com.example.mgp2023;
 
-public class LoseScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class LoseScreen extends Activity implements OnClickListener, StateBase
+{
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public String GetName() {
+        return null;
+    }
+
+    @Override
+    public void OnEnter(SurfaceView _view) {
+
+    }
+
+    @Override
+    public void OnExit() {
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas) {
+
+    }
+
+    @Override
+    public void Update(float _dt) {
+
+    }
 }
